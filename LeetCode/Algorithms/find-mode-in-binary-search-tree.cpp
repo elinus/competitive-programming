363f33ca23de036143c62e8cad2c68085ea8b1c8
Sunil Mourya <sunilmourya.cse.nitp@gmail.com>
2020-04-15 21:50:17 +0530

    [15042020_2150] LeetCode

---
+++ LeetCode/Algorithms/find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    void traverse(TreeNode *root, map<int, int> & m) {
+        if (root) {
+            if (m.find(root->val) != m.end())
+                m[root->val] += 1;
+            else 
+                m[root->val] = 1;
+            traverse(root->left, m);
+            traverse(root->right, m);
+        }
+    }
+public:
+    vector<int> findMode(TreeNode* root) {
+        map<int, int> m;
+        traverse(root, m);
+        int max = INT_MIN;
+        for (auto i = m.begin(); i != m.end(); ++i) {
+            if (i->second > max) max = i->second; 
+        }
+        vector<int> answer;
+        for (auto i = m.begin(); i != m.end(); ++i) {
+            if (i->second == max) answer.push_back(i->first); 
+        }
+        return answer;
+    }
+};
