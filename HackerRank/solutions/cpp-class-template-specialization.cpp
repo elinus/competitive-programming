ef02499511affb775a503d31df7ad4e091e748b4
Sunil Mourya <sunilmourya.nitp.cse@gmail.com>
2020-08-04 23:15:27 +0530

    Random - 20200804_2315

--- HackerRank/solutions/cpp-class-template-specialization.cpp
+++ HackerRank/solutions/cpp-class-template-specialization.cpp
@@ -14,32 +14,32 @@ char c2[] = "orange";
 
 template <>
 struct Traits<Fruit> {
-public:
-    static char* name(int a) {
-        if(a >= 3 || a < 0)
-            return s;
-        else if (a == 0)
-            return f0;
-        else if (a == 1)
-            return f1;
-        else
-            return f2;            
-    }
+	public:
+		static char* name(int a) {
+			if(a >= 3 || a < 0)
+				return s;
+			else if (a == 0)
+				return f0;
+			else if (a == 1)
+				return f1;
+			else
+				return f2;            
+		}
 };
 
 template <>
 struct Traits<Color> {
-public:
-    static char* name(int a) {
-        if(a >= 3 || a < 0)
-            return s;
-        else if (a == 0)
-            return c0;
-        else if (a == 1)
-            return c1;
-        else
-            return c2;  
-    }
+	public:
+		static char* name(int a) {
+			if(a >= 3 || a < 0)
+				return s;
+			else if (a == 0)
+				return c0;
+			else if (a == 1)
+				return c1;
+			else
+				return c2;  
+		}
 };
 
 
@@ -47,10 +47,10 @@ int main()
 {
 	int t = 0; std::cin >> t;
 
-    for (int i=0; i!=t; ++i) {
-        int index1; std::cin >> index1;
-        int index2; std::cin >> index2;
-        cout << Traits<Color>::name(index1) << " ";
-        cout << Traits<Fruit>::name(index2) << "\n";
-    }
+	for (int i=0; i!=t; ++i) {
+		int index1; std::cin >> index1;
+		int index2; std::cin >> index2;
+		cout << Traits<Color>::name(index1) << " ";
+		cout << Traits<Fruit>::name(index2) << "\n";
+	}
 }
